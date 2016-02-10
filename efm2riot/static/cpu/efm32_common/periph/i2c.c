1d00cc36e434c2acdcfd5e0e320a42fd4f2bccfb
Bas Stottelaar <basstottelaar@gmail.com>
2016-02-10 20:43:56 +0100

    Add i2c interrupt-based transfer.

--- efm2riot/static/cpu/efm32_common/periph/i2c.c
+++ efm2riot/static/cpu/efm32_common/periph/i2c.c
@@ -25,7 +25,7 @@
 #include "periph/i2c.h"
 #include "periph/gpio.h"
 
-/* emlib uses the same flags */
+/* emlib uses the same flags, undefine fist */
 #undef I2C_FLAG_WRITE
 #undef I2C_FLAG_READ
 
@@ -34,6 +34,13 @@
 
 /* guard file in case no I2C device is defined */
 #if I2C_NUMOF
+
+typedef struct {
+    I2C_TransferReturn_TypeDef progress;     /**< transfer progress */
+    mutex_t lock;                            /**< peripheral lock */
+} i2c_state_t;
+
+static i2c_state_t i2c_state[I2C_NUMOF];
 
 /**
  * @brief Convert speeds to integers
@@ -63,22 +70,21 @@ static uint32_t speed_to_freq(i2c_speed_t speed)
     return freq;
 }
 
-static I2C_TransferReturn_TypeDef _transfer(I2C_TypeDef *dev,
-                                            I2C_TransferSeq_TypeDef *transfer)
+/**
+ * @brief   Start and track an I2C transfer.
+ */
+static void _transfer(i2c_t dev, I2C_TransferSeq_TypeDef *transfer)
 {
-    I2C_TransferReturn_TypeDef progress = I2C_TransferInit(dev, transfer);
+    i2c_state[dev].progress = I2C_TransferInit(i2c_config[dev].dev, transfer);
 
-    while (progress == i2cTransferInProgress) {
-        progress = I2C_Transfer(dev);
+    while (i2c_state[dev].progress == i2cTransferInProgress) {
+        /* the transfer progresses via the interrupt handler */
+        __WFI();
     }
-
-    return progress;
 }
 
 int i2c_init_master(i2c_t dev, i2c_speed_t speed)
 {
-    I2C_Init_TypeDef init = I2C_INIT_DEFAULT;
-
     /* check if device is valid */
     if (dev >= I2C_NUMOF) {
         return -1;
@@ -88,41 +94,52 @@ int i2c_init_master(i2c_t dev, i2c_speed_t speed)
     gpio_init(i2c_config[dev].scl_pin, GPIO_DIR_BI, GPIO_NOPULL);
     gpio_init(i2c_config[dev].sda_pin, GPIO_DIR_BI, GPIO_NOPULL);
 
+    /* ensure slave is in a known state, which it may not after a reset */
+    for (int i = 0; i < 9; i++) {
+        gpio_set(i2c_config[dev].scl_pin);
+        gpio_clear(i2c_config[dev].scl_pin);
+    }
+
     /* enable clock */
     CMU_ClockEnable(cmuClock_HFPER, true);
     CMU_ClockEnable(i2c_config[dev].cmu, true);
 
-    /* initialize the device */
-    init.enable = false;
+    /* reset the device */
+    I2C_Reset(i2c_config[dev].dev);
+
+    /* enable interrupts */
+    NVIC_ClearPendingIRQ(i2c_config[dev].irq);
+    NVIC_EnableIRQ(i2c_config[dev].irq);
+
+    /* initialize and enable the device */
+    I2C_Init_TypeDef init = I2C_INIT_DEFAULT;
+
     init.freq = speed_to_freq(speed);
 
-    I2C_Reset(i2c_config[dev].dev);
     I2C_Init(i2c_config[dev].dev, &init);
 
-#ifdef _SILICON_LABS_32B_PLATFORM_1
-    i2c_config[dev].dev->ROUTE = (i2c_config[dev].loc |
-                                  I2C_ROUTE_SDAPEN | I2C_ROUTE_SCLPEN);
-#else
-    i2c_config[dev].dev->ROUTEPEN = I2C_ROUTEPEN_SDAPEN | I2C_ROUTEPEN_SCLPEN;
-    i2c_config[dev].dev->ROUTELOC0 = i2c_config[dev].loc;
-#endif
-
-    /* enable it */
-    I2C_Enable(i2c_config[dev].dev, true);
+    /* configure pins to output I2C */
+    #ifdef _SILICON_LABS_32B_PLATFORM_1
+        i2c_config[dev].dev->ROUTE = (i2c_config[dev].loc |
+                                      I2C_ROUTE_SDAPEN | I2C_ROUTE_SCLPEN);
+    #else
+        i2c_config[dev].dev->ROUTEPEN = I2C_ROUTEPEN_SDAPEN | I2C_ROUTEPEN_SCLPEN;
+        i2c_config[dev].dev->ROUTELOC0 = i2c_config[dev].loc;
+    #endif
 
     return 0;
 }
 
 int i2c_acquire(i2c_t dev)
 {
-    mutex_lock((mutex_t *) &i2c_config[dev].lock);
+    mutex_lock((mutex_t *) &i2c_state[dev].lock);
 
     return 0;
 }
 
 int i2c_release(i2c_t dev)
 {
-    mutex_unlock((mutex_t *) &i2c_config[dev].lock);
+    mutex_unlock((mutex_t *) &i2c_state[dev].lock);
 
     return 0;
 }
@@ -141,8 +158,10 @@ int i2c_read_bytes(i2c_t dev, uint8_t address, char *data, int length)
     transfer.buf[0].data = (uint8_t *) data;
     transfer.buf[0].len = length;
 
-    /* transfer data using polling method */
-    if (_transfer(i2c_config[dev].dev, &transfer) != i2cTransferDone) {
+    /* start a transfer */
+    _transfer(dev, &transfer);
+
+    if (i2c_state[dev].progress != i2cTransferDone) {
         return -2;
     }
 
@@ -166,8 +185,10 @@ int i2c_read_regs(i2c_t dev, uint8_t address, uint8_t reg,
     transfer.buf[1].data = (uint8_t *) data;
     transfer.buf[1].len = length;
 
-    /* transfer data using polling method */
-    if (_transfer(i2c_config[dev].dev, &transfer) != i2cTransferDone) {
+    /* start a transfer */
+    _transfer(dev, &transfer);
+
+    if (i2c_state[dev].progress != i2cTransferDone) {
         return -2;
     }
 
@@ -188,8 +209,10 @@ int i2c_write_bytes(i2c_t dev, uint8_t address, char *data, int length)
     transfer.buf[0].data = (uint8_t *) data;
     transfer.buf[0].len = length;
 
-    /* transfer data using polling method */
-    if (_transfer(i2c_config[dev].dev, &transfer) != i2cTransferDone) {
+    /* start a transfer */
+    _transfer(dev, &transfer);
+
+    if (i2c_state[dev].progress != i2cTransferDone) {
         return -2;
     }
 
@@ -213,8 +236,10 @@ int i2c_write_regs(i2c_t dev, uint8_t address, uint8_t reg,
     transfer.buf[1].data = (uint8_t *) data;
     transfer.buf[1].len = length;
 
-    /* transfer data using polling method */
-    if (_transfer(i2c_config[dev].dev, &transfer) != i2cTransferDone) {
+    /* start a transfer */
+    _transfer(dev, &transfer);
+
+    if (i2c_state[dev].progress != i2cTransferDone) {
         return -2;
     }
 
@@ -230,5 +255,19 @@ void i2c_poweroff(i2c_t dev)
 {
     CMU_ClockEnable(i2c_config[dev].cmu, false);
 }
+
+#ifdef I2C_0_ISR
+void I2C_0_ISR(void)
+{
+    i2c_state[0].progress = I2C_Transfer(i2c_config[0].dev);
+}
+#endif
+
+#ifdef I2C_1_ISR
+void I2C_1_ISR(void)
+{
+    i2c_state[1].progress = I2C_Transfer(i2c_config[1].dev);
+}
+#endif
 
 #endif /* I2C_NUMOF */
