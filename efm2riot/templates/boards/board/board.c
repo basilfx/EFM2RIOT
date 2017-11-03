37b63c2d3ff554cd7a10eefe50f1aea6da760cbf
Bas Stottelaar <basstottelaar@gmail.com>
2017-11-03 19:00:48 +0100

    Break long lines.

--- efm2riot/templates/boards/board/board.c
+++ efm2riot/templates/boards/board/board.c
@@ -40,11 +40,14 @@
 
 {% strip 2, ">" %}
     {% if board in ["sltb001a"] %}
-        #if BMP280_ENABLED || CCS811_ENABLED || ICM_20648_ENABLED || SI1133_ENABLED || SI7021_ENABLED || SI7210A_ENABLED || RGB_LED1_ENABLED || RGB_LED2_ENABLED || RGB_LED3_ENABLED || RGB_LED4_ENABLED
+        #if BMP280_ENABLED || CCS811_ENABLED || ICM_20648_ENABLED || \
+            SI1133_ENABLED || SI7021_ENABLED || SI7210A_ENABLED || \
+            RGB_LED1_ENABLED || RGB_LED2_ENABLED || RGB_LED3_ENABLED || \
+            RGB_LED4_ENABLED
         static inline void board_usleep(uint32_t delay)
         {
             delay = (delay * 1000) / (1000 / (SystemCoreClock / 1000 / 1000));
-            
+
             while (delay--) {
                 __asm("nop");
             }
@@ -54,7 +57,7 @@
         {
             gpio_init(PIC_INT_WAKE_PIN, GPIO_OD);
             gpio_set(PIC_INT_WAKE_PIN);
-            
+
             i2c_init_master(PIC_I2C, I2C_SPEED_NORMAL);
         }
 
@@ -63,13 +66,13 @@
             /* toggle the pin for 4 us */
             gpio_clear(PIC_INT_WAKE_PIN);
             board_usleep(4);
-            
+
             /* write to gpio expander */
-            i2c_acquire(PIC_I2C); 
+            i2c_acquire(PIC_I2C);
             uint8_t bytes[] = { addr, value };
             i2c_write_bytes(PIC_I2C, PIC_I2C_ADDR, bytes, 2);
             i2c_release(PIC_I2C);
-            
+
             /* put PIC in sleep mode again */
             gpio_set(PIC_INT_WAKE_PIN);
         }
@@ -156,32 +159,35 @@ void board_init(void)
     {% strip 3, "<" %}
         {% if board in ["sltb001a"] %}
                 /* initialize the environmental sensors (multiple ones) */
-            #if BMP280_ENABLED || CCS811_ENABLED || ICM_20648_ENABLED || SI1133_ENABLED || SI7021_ENABLED || SI7210A_ENABLED || RGB_LED1_ENABLED || RGB_LED2_ENABLED || RGB_LED3_ENABLED || RGB_LED4_ENABLED
+            #if BMP280_ENABLED || CCS811_ENABLED || ICM_20648_ENABLED || \
+                SI1133_ENABLED || SI7021_ENABLED || SI7210A_ENABLED || \
+                RGB_LED1_ENABLED || RGB_LED2_ENABLED || RGB_LED3_ENABLED || \
+                RGB_LED4_ENABLED
                 board_pic_init();
             #endif
-            
+
                 /* enable the CCS811 air quality/gas sensor */
             #if CCS811_ENABLED
                 board_pic_write(CCS811_PIC_ADDR, (1 << CCS811_PIC_EN_BIT) | (1 << CCS811_PIC_WAKE_BIT));
             #endif
-            
+
                 /* enable the IMU sensor */
             #if ICM_20648_ENABLED
                 board_pic_write(ICM20648_PIC_ADDR, 1 << ICM20648_PIC_EN_BIT);
             #endif
-            
+
                 /* enable the environmental sensors */
             #if BMP280_ENABLED || SI1133_ENABLED || SI7021_ENABLED || SI7210A_ENABLED
                 board_pic_write(ENV_SENSE_PIC_ADDR, 1 << ENV_SENSE_PIC_BIT);
             #endif
-            
+
                 /* enable the RGB leds */
             #if RGB_LED1_ENABLED || RGB_LED2_ENABLED || RGB_LED3_ENABLED || RGB_LED4_ENABLED
-                board_pic_write(RGB_LED_ADDR, 
-                    (1 << RGB_LED_EN_BIT) | 
-                    (RGB_LED1_ENABLED << RGB_LED1_EN_BIT) | 
-                    (RGB_LED2_ENABLED << RGB_LED2_EN_BIT) | 
-                    (RGB_LED3_ENABLED << RGB_LED3_EN_BIT) | 
+                board_pic_write(RGB_LED_ADDR,
+                    (1 << RGB_LED_EN_BIT) |
+                    (RGB_LED1_ENABLED << RGB_LED1_EN_BIT) |
+                    (RGB_LED2_ENABLED << RGB_LED2_EN_BIT) |
+                    (RGB_LED3_ENABLED << RGB_LED3_EN_BIT) |
                     (RGB_LED4_ENABLED << RGB_LED4_EN_BIT));
             #endif
         {% endif %}
