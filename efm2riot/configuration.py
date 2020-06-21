af9ef4c7fcf331991085723d60ce241256611e43
Bas Stottelaar <basstottelaar@gmail.com>
2020-06-21 23:53:45 +0200

    Check all URLs and remove schematics since most of them are 404.

--- efm2riot/configuration.py
+++ efm2riot/configuration.py
@@ -125,7 +125,6 @@ BOARDS = [
         "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32tg11-datasheet.pdf",  # noqa
         "board": "slstk3301a",
         "board_manual": "https://www.silabs.com/documents/public/user-guides/ug303-stk3301.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/documents/public/schematic-files/BRD2102A-A04-schematic.pdf",
         "family": "efm32tg11b",
         "family_full_name": "EFM32 Tiny Gecko 11B",
         "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efm32tg11-rm.pdf",  # noqa
@@ -141,13 +140,12 @@ BOARDS = [
     },
     {
         "cpu": "efm32pg1b200f256gm48",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32PG1-DataSheet.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32pg1-datasheet.pdf",  # noqa
         "board": "slstk3401a",
         "board_manual": "https://www.silabs.com/documents/public/user-guides/ug154-stk3401-user-guide.pdf",  # noqa
-        "board_schematic": "",
         "family": "efm32pg1b",
         "family_full_name": "EFM32 Pearl Gecko 1B",
-        "family_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32PG1-ReferenceManual.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efm32pg1-rm.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 40000000,
@@ -163,7 +161,6 @@ BOARDS = [
         "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32pg12-datasheet.pdf",  # noqa
         "board": "slstk3402a",
         "board_manual": "https://www.silabs.com/documents/public/user-guides/ug257-stk3402-usersguide.pdf",  # noqa
-        "board_schematic": "",
         "family": "efm32pg12b",
         "family_full_name": "EFM32 Pearl Gecko 12B",
         "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efm32pg12-rm.pdf",  # noqa
@@ -182,7 +179,6 @@ BOARDS = [
         "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32gg11-datasheet.pdf",  # noqa
         "board": "slstk3701a",
         "board_manual": "https://www.silabs.com/documents/public/user-guides/ug287-stk3701.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/documents/public/schematic-files/BRD2204A-B00-schematic.pdf",
         "family": "efm32gg11b",
         "family_full_name": "EFM32 Giant Gecko 11B",
         "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efm32gg11-rm.pdf",  # noqa
@@ -198,13 +194,12 @@ BOARDS = [
     },
     {
         "cpu": "efr32mg1p132f256gm48",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFR32MG1-DataSheet.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efr32mg1-datasheet.pdf",  # noqa
         "board": "sltb001a",
-        "board_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/UG250.pdf",  # noqa
-        "board_schematic": "http://www.silabs.com/Support%20Documents/TechnicalDocs/UG250.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/ug250-tb001-user-guide.pdf",  # noqa
         "family": "efr32mg1p",
         "family_full_name": "EFR32 Mighty Gecko 1P",
-        "family_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/EFR32-ReferenceManual.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efr32xg1-rm.pdf",  # noqa
         "features": [],
         "freq": {
             "hfxo": 38400000,
@@ -219,11 +214,10 @@ BOARDS = [
         "cpu": "efr32mg12p332f1024gl125",
         "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efr32mg12-datasheet.pdf",  # noqa
         "board": "slwstk6000b",
-        "board_manual": "https://www.silabs.com/documents/public/reference-manuals/slwstk6000b-rm.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/documents/public/reference-manuals/slwstk6000b-rm.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/ug260-brd4161a.pdf",  # noqa
         "family": "efr32mg12p",
         "family_full_name": "EFR32 Mighty Gecko 12P",
-        "family_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/EFR32-ReferenceManual.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/efr32xg12-rm.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 38400000,
@@ -236,13 +230,12 @@ BOARDS = [
     },
     {
         "cpu": "ezr32wg330f256r60",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EZR32WG330_DataSheet.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/EZR32WG330_DataSheet.pdf",  # noqa
         "board": "slwstk6220a",
-        "board_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/wstk6220_EZR32WG_868MHz_Wireless_Starter_Kit.pdf",  # noqa
-        "board_schematic": "http://www.silabs.com/Support%20Documents/TechnicalDocs/BRD2400A_A00.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/ug200-brd4502c-user-guide.pdf",  # noqa
         "family": "ezr32wg",
         "family_full_name": "EZR32 Wonder Gecko",
-        "family_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/EZR32LG-RM.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/EZR32LG-RM.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 48000000,
@@ -255,12 +248,11 @@ BOARDS = [
     },
     {
         "cpu": "efm32zg222f32",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32ZG222.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32zg-datasheet.pdf",  # noqa
         "board": "stk3200",
-        "board_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/efm32zg-stk3200-ug.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/Support%20Documents/TechnicalDocs/BRD2010A_B01.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/efm32zg-stk3200-ug.pdf",  # noqa
         "family_full_name": "EFM32 Zero Gecko",
-        "family_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32ZG-RM.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/EFM32ZG-RM.pdf",  # noqa
         "features": ["bc"],
         "freq": {
             "hfxo": 24000000,
@@ -273,13 +265,12 @@ BOARDS = [
     },
     {
         "cpu": "efm32lg990f256",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32LG990.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32lg-datasheet.pdf",  # noqa
         "board": "stk3600",
-        "board_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/efm32lg-stk3600-ug.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/Support%20Documents/TechnicalDocs/BRD2201A_A02.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/efm32lg-stk3600-ug.pdf",  # noqa
         "family": "efm32lg",
         "family_full_name": "EFM32 Leoppard Gecko",
-        "family_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32LG-RM.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/EFM32LG-RM.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 48000000,
@@ -292,13 +283,12 @@ BOARDS = [
     },
     {
         "cpu": "efm32gg990f1024",
-        "cpu_datasheet": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32GG990.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32gg-datasheet.pdf",  # noqa
         "board": "stk3700",
-        "board_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/efm32gg-stk3700-ug.pdf",  # noqa
-        "board_schematic": "https://www.silabs.com/Support%20Documents/TechnicalDocs/BRD2200A_A03.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/efm32gg-stk3700-ug.pdf",  # noqa
         "family": "efm32gg",
         "family_full_name": "EFM32 Giant Gecko",
-        "family_manual": "https://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32GG-RM.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/EFM32GG-RM.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 48000000,
@@ -311,13 +301,12 @@ BOARDS = [
     },
     {
         "cpu": "efm32wg990f256",
-        "cpu_datasheet": "http://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32WG990.pdf",  # noqa
+        "cpu_datasheet": "https://www.silabs.com/documents/public/data-sheets/efm32wg-datasheet.pdf",  # noqa
         "board": "stk3800",
-        "board_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/efm32wg-stk3800-ug.pdf",  # noqa
-        "board_schematic": "http://www.silabs.com/Support%20Documents/TechnicalDocs/BRD2400A_A00.pdf",  # noqa
+        "board_manual": "https://www.silabs.com/documents/public/user-guides/efm32wg-stk3800-ug.pdf",  # noqa
         "family": "efm32wg",
         "family_full_name": "EFM32 Wonder Gecko",
-        "family_manual": "http://www.silabs.com/Support%20Documents/TechnicalDocs/EFM32WG-RM.pdf",  # noqa
+        "family_manual": "https://www.silabs.com/documents/public/reference-manuals/EFM32WG-RM.pdf",  # noqa
         "features": ["aem", "bc"],
         "freq": {
             "hfxo": 48000000,
