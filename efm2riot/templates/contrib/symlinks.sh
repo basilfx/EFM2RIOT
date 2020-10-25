0b832df614a2e10ba06e998f7c242261d899834b
Bas Stottelaar <basstottelaar@gmail.com>
2020-10-25 15:49:25 +0100

    Use $(..) instead of backticks.

--- efm2riot/templates/contrib/symlinks.sh
+++ efm2riot/templates/contrib/symlinks.sh
@@ -3,17 +3,17 @@
 BOARDS="{{ boards|join(" ") }}"
 FAMILIES="{{ families|join(" ") }}"
 
-SOURCE=`dirname $0`/..
+SOURCE=$(dirname "$0")/..
 TARGET=$1
 
 # Symlink all boards into `RIOT_ROOT/boards/`.
 for board in $BOARDS
 do
-    ln -sf `realpath "$SOURCE/boards/$board"` "$TARGET/boards/"
+    ln -sf "$(realpath "$SOURCE/boards/$board")" "$TARGET/boards/$board"
 done
 
 # Symlink the families folder into `RIOT_ROOT/cpu/families/`.
 for family in $FAMILIES
 do
-    ln -sf `realpath "$SOURCE/cpu/efm32/families/$family"` "$TARGET/cpu/efm32/families/"
+    ln -sf "$(realpath "$SOURCE/cpu/efm32/families/$family")" "$TARGET/cpu/efm32/families/$family"
 done
